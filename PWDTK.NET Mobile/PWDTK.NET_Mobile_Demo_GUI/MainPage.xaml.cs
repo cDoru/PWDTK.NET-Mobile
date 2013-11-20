d80af6ff4b02266a7a664c9e1cce3b21b98fa5a4
Thashiznets <hdizzle@outlook.com>
2013-11-20 22:51:22 +1100

    Initial v1.0.0.0

---
+++ PWDTK.NET Mobile/PWDTK.NET_Mobile_Demo_GUI/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using PWDTK_MOBILE_WP_8;
+
+namespace PWDTK_Mobile_Demo_GUI
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        Byte[] salt;
+        Byte[] hash;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+           salt = PWDTK.GetRandomSalt(PWDTK.cDefaultSaltLength);
+           hash = PWDTK.PasswordToHash(salt, textBox1.Text, PWDTK.cDefaultIterationCount);
+           //Now store the salt and password Byte[] somewhere perhaps if WP has an implementation of iOS keychain?
+           MessageBox.Show("Salt: "+PWDTK.HashBytesToHexString(salt)+"\n\nPassword: "+textBox1.Text+"\n\nHash: "+PWDTK.HashBytesToHexString(hash));
+           button2.IsEnabled = true;
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (PWDTK.ComparePasswordToHash(salt, textBox1.Text, hash))
+            {
+                MessageBox.Show("We have a match :)\n\nSalt: " + PWDTK.HashBytesToHexString(salt) + "\n\nPassword: " + textBox1.Text + "\n\nCreated Hash: " + PWDTK.HashBytesToHexString(PWDTK.PasswordToHash(salt, textBox1.Text, PWDTK.cDefaultIterationCount)) + "\n\nStored Hash: " + PWDTK.HashBytesToHexString(hash));
+            }
+            else
+            {
+                MessageBox.Show("Hash generated doesn't match stored password :( Deny Deny Deny Deny Deny!!!!\n\nSalt: " + PWDTK.HashBytesToHexString(salt) + "\n\nPassword: " + textBox1.Text + "\n\nCreated Hash: " + PWDTK.HashBytesToHexString(PWDTK.PasswordToHash(salt, textBox1.Text, PWDTK.cDefaultIterationCount)) + "\n\nStored Hash: " + PWDTK.HashBytesToHexString(hash));
+            }
+        }
+    }
+}
---
+++ PWDTK.NET Mobile/PWDTK.NET_Mobile_Demo_GUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PWDTK_Mobile_Demo_GUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ian Harris")]
+[assembly: AssemblyProduct("PWDTK_Mobile_Demo_GUI")]
+[assembly: AssemblyCopyright("Ian Harris")]
+[assembly: AssemblyTrademark("PWDTK")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("281f298b-c7ff-48bb-a308-cc973d688e38")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
